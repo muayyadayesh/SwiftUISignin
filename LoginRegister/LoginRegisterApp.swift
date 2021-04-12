bc273f87424e3eea20411ec092f783138ce10b82
muayyadayesh <muayyadayesh@gmail.com>
2021-04-12 21:15:43 +0300

    Initial Commit

---
+++ LoginRegister/LoginRegisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginRegisterApp.swift
+//  LoginRegister
+//
+//  Created by mac book pro on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginRegisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
